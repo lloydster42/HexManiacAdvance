0d93465e4be067a7aef04f85323c6fccdec33f77
Benjamin Popp <haven1433@gmail.com>
2021-11-18 13:54:16 -0600

    Get the application running
    Everything seems to work now. But the dll/pdb/xml files can't be placed in another directory, making the directory 11 files wider than it was before. I don't know how to fix this.

--- src/HexManiac.WPF/Windows/App.xaml.cs
+++ src/HexManiac.WPF/Windows/App.xaml.cs
@@ -33,6 +33,7 @@ namespace HavenSoft.HexManiac.WPF.Windows {
       public static void Main(string[] args) => new App().Run();
 
       public App() {
+         this.InitializeComponent();
          // name mutex and pipes based on the file location.
          // This allows us to have debug and release running at the same time,
          // or 0.3 and 0.4 running at the same time, etc.
